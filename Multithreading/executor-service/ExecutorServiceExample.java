db86ebe5ad82aad4dd95e7c46e8cfee69b72918c
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2025-01-07 10:50:24 +0530

    Update ExecutorServiceExample.java

--- Multithreading/executor-service/ExecutorServiceExample.java
+++ Multithreading/executor-service/ExecutorServiceExample.java
@@ -1,5 +1,19 @@
-class ExecutorServiceExample {
-  public static void main(String[] args){
-    
-  }
+// "static void main" must be defined in a public class.
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        
+        for(int i = 1 ; i <= 5; i++){
+            int task = i;
+            executor.execute(() -> {
+                System.out.println("Executing Task " + task + " by " + Thread.currentThread().getName());
+            });
+        }
+        
+        executor.shutdown();
+        
+    }
 }

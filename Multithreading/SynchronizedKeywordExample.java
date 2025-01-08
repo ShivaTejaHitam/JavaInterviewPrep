f21082a1666af471a9f2f7350910af7eedc4c095
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2025-01-08 13:11:00 +0530

    Create SynchronizedKeywordExample.java

---
+++ Multithreading/SynchronizedKeywordExample.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Resource {
+    static synchronized void perform(){
+        try{
+            for(int i = 0 ; i < 5 ; i++){
+                Thread.sleep(1000);
+                System.out.println("This is sensitive"+ Thread.currentThread().getName());
+            }
+        }catch(InterruptedException e){
+            System.out.println("InterruptedException");
+        }
+    }
+
+    static synchronized void useLess(){
+        try{
+            for(int i = 0 ; i < 5 ; i++){
+                Thread.sleep(1000);
+                System.out.println("after useless sleep"+ Thread.currentThread().getName());
+            }
+        }catch(InterruptedException e){
+            System.out.println("InterruptedException");
+        }
+    }
+}
+
+
+class Interview
+{
+    public static void main(String args[])
+    {
+
+       Thread t1 = new Thread(() -> {
+            Resource.useLess();
+       });
+
+       Thread t2 = new Thread(() -> {
+            Resource.perform();
+       });
+
+       t1.start();
+       t2.start();
+
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+

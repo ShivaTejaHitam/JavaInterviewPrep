09ee5c309814a65f99a3ff4b686e309675942ffa
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2026-03-11 14:41:00 +0530

    Update ProducerConsumerProblem.java

--- Multithreading/problems-and-solutions/ProducerConsumerProblem.java
+++ Multithreading/problems-and-solutions/ProducerConsumerProblem.java
@@ -1,1 +1,62 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
+class SharedResource {
+    private Queue<Integer> buffer = new LinkedList<>();
+    private final int CAPACITY = 5;
+
+    public void produce(int value) throws InterruptedException {
+        synchronized(this){
+            while(buffer.size() == CAPACITY) {
+                wait();
+            }
+
+            buffer.add(value);
+            System.out.println("Produced: " + value);
+            notify();
+        }
+    }
+
+    public void consume() throws InterruptedException{
+        synchronized(this){
+            while(buffer.isEmpty()) {
+                wait();
+            }
+
+            int value = buffer.poll();
+            System.out.println("Consumed: " + value);
+            notify();
+        }
+    }
+}
+
+class InterThreadCommunication {
+    public static void main(String[] args) {
+        SharedResource sharedResource = new SharedResource();
+
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {// replace with while(true) for infinite production
+                    sharedResource.produce(i);
+                    Thread.sleep(100); 
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) { // replace with while(true) for infinite consumption
+                    sharedResource.consume();
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+    }
+}
